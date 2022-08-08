783c15b8d365485bbc32fddb413a78b49b6f1756
ShinYoung hong <gtd4095@naver.com>
2022-08-08 23:28:33 +0900

    8.8
    player:
    움직임, 느리게걷기, 방향보기
    Follow:
    카메라 쿼터뷰 구현

--- Player.cs
+++ Player.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public float speed;
+    float hAxis;
+    float vAxis;
+    bool wDown;
+
+
+    Vector3 moveVec;
+
+    Animator anim;
+
+    void Awake()
     {
-        
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+        wDown = Input.GetButton("Walk");
+
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        transform.position += moveVec * speed * (wDown?0.3f:1f) * Time.deltaTime;
+
+        anim.SetBool("isRun", moveVec != Vector3.zero);
+        anim.SetBool("isWalk", wDown);
+
+        transform.LookAt(transform.position + moveVec);
+
     }
 }
